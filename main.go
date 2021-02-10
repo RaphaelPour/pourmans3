3fcaf39023dfd03174d66b116477bf76ac7e9814
Raphael Pour <info@raphaelpour.de>
2021-02-10 19:07:41 +0100

    Repo: Simplify code

--- main.go
+++ main.go
@@ -6,18 +6,21 @@ import (
 	"hash/maphash"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 const (
-	PORT          = 80
 	POST_TEMPLATE = `
 	<html>
+	<head><title>pourmans3</title></head>
 	<body>
+	<h1 style='text-align:left'>pourmans3</h1>
+	<h3 style='text-align:left'><em>Link shortener, designed to store heave-payloaded urls</em></h3>
+	<hr>
 	<form method='POST'>
-	URL:<input type='text' name='url'>
+	<input type='text' name='url'>
 	<input type='submit' value='Shorten'>
 	</form>
-	<hr>
 	<ul>
 	{{ range $short, $long := .Links }}
 		<li><a href='?{{ $short }}'>{{ $short }}</a> -> <a href='{{ $long }}'>{{ $long }}</a></li>
@@ -38,11 +41,14 @@ const (
 var (
 	BuildDate    string
 	BuildVersion string
+	Port         = flag.Int("port", 80, "Listen port")
 	Version      = flag.Bool("version", false, "Print build information")
 	hash         maphash.Hash
 	links        = map[string]string{
 		"about": "https://evilcookie.de",
 	}
+	postTemplate *template.Template
+	getTemplate  *template.Template
 )
 
 type ShortLink struct {
@@ -74,56 +80,71 @@ func main() {
 		return
 	}
 
-	postTemplate, err := template.New("post").Parse(POST_TEMPLATE)
+	var err error
+	postTemplate, err = template.New("post").Parse(POST_TEMPLATE)
 	if err != nil {
 		fmt.Println("error parsing post template:", err)
 		return
 	}
 
-	getTemplate, err := template.New("get").Parse(GET_TEMPLATE)
+	getTemplate, err = template.New("get").Parse(GET_TEMPLATE)
 	if err != nil {
 		fmt.Println("error parsing get template:", err)
 		return
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		hosts := r.Header["Refferer"]
-		var host string
-		if len(hosts) == 0 {
-			host = "localhost"
-		} else {
-			host = hosts[0]
-		}
+	http.HandleFunc("/", RequestHandler)
 
-		if r.Method == http.MethodPost {
-			if r.FormValue("url") == "" {
-				fmt.Println("error on post: url parameter missing")
-				return
-			}
-			short := shorten(r.FormValue("url"))
-			links[short] = r.FormValue("url")
+	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%d", *Port), nil))
+}
 
-			fmt.Fprintf(w, "%s/?%s", host, short)
+func RequestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf(
+		"%s [%6s] %s\n",
+		time.Now().UTC().Format("2006-01-02T15:04:05"),
+		r.Method,
+		r.URL,
+	)
+
+	hosts := r.Header["Refferer"]
+	var host string
+	if len(hosts) == 0 {
+		host = "localhost"
+	} else {
+		host = hosts[0]
+	}
+
+	if r.Method == http.MethodPost {
+		if r.FormValue("url") == "" {
+			fmt.Println("error on post: url parameter missing")
 			return
 		}
+		short := shorten(r.FormValue("url"))
+		links[short] = r.FormValue("url")
 
-		if r.Method == http.MethodGet && r.URL.RawQuery != "" {
+		fmt.Fprintf(w, "%s/?%s", host, short)
+		return
+	}
 
-			if err := getTemplate.Execute(w, extend(r.URL.RawQuery)); err != nil {
-				fmt.Println("error rendering get template:", err)
-			}
-			return
+	if r.Method == http.MethodGet && r.URL.RawQuery != "" {
+
+		if err := getTemplate.Execute(w, extend(r.URL.RawQuery)); err != nil {
+			fmt.Println("error rendering get template:", err)
 		}
+		return
+	}
 
-		if err := postTemplate.Execute(w,
-			ShortLink{
-				Links: links,
-				Host:  host,
-			}); err != nil {
-			fmt.Println("error rendering post template:", err)
-			return
-		}
-	})
+	if r.Method == http.MethodDelete && r.URL.RawQuery != "" {
+		delete(links, r.URL.RawQuery)
+		return
+	}
 
-	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
+	if err := postTemplate.Execute(w,
+		ShortLink{
+			Links: links,
+			Host:  host,
+		}); err != nil {
+		fmt.Println("error rendering post template:", err)
+		return
+	}
 }
