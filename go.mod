8e28e9ae3e1f9119ed255349b0c5ef4ce0107737
Raphael Pour <info@raphaelpour.de>
2021-02-10 18:05:03 +0100

    Add function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RaphaelPour/pourmans3
+
+go 1.15
--- main.go
+++ main.go
@@ -1,31 +1,133 @@
-package main
+package main // import "github.com/RaphaelPour/pourmans3"
 
-import(
+import (
+	"flag"
 	"fmt"
-	"net/http"
-	"log"
 	"hash/maphash"
-	"encoding/base64"
+	"html/template"
+	"net/http"
 )
 
-var hash maphash.Hash
-var links = map[string]string{
-	"123":"234",
-}
+const (
+	PORT          = 80
+	POST_TEMPLATE = `
+	<html>
+	<body>
+	<form method='POST'>
+	URL:<input type='text' name='url'>
+	<input type='submit' value='Shorten'>
+	</form>
+	<hr>
+	<ul>
+	{{ range $short, $long := .Links }}
+		<li><a href='?{{ $short }}'>{{ $short }}</a> -> <a href='{{ $long }}'>{{ $long }}</a></li>
+	{{ end }}
+	</ul>
+	</body>
+	</html>
+	`
+	GET_TEMPLATE = `
+	<html>
+	<head>
+		<meta http-equiv="Refresh" content="0; URL={{ . }}">
+	</head>
+	</html>
+	`
+)
+
+var (
+	BuildDate    string
+	BuildVersion string
+	Version      = flag.Bool("version", false, "Print build information")
+	hash         maphash.Hash
+	links        = map[string]string{
+		"about": "https://evilcookie.de",
+	}
+)
 
 func shorten(link string) string {
 	hash.Reset()
 	hash.WriteString(link)
-	return base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%x",hash.Sum64())))
+	return fmt.Sprintf("%x", hash.Sum64())
+}
+
+func extend(short string) string {
+	if long, ok := links[short]; ok {
+		return long
+	}
+	fmt.Println("unknown short link ", short)
+	return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		for short, long := range links {
-			fmt.Fprintf(w, "%s -> %s\n", short, long)
+	flag.Parse()
+
+	if *Version {
+		fmt.Println("BuildVersion: ", BuildVersion)
+		fmt.Println("BuildDate: ", BuildDate)
+		return
+	}
+
+	postTemplate, err := template.New("test").Parse(POST_TEMPLATE)
+	if err != nil {
+		fmt.Println("error parsing html template:", err)
+		return
+	}
+
+	getTemplate, err := template.New("redirrect").Parse(GET_TEMPLATE)
+	if err != nil {
+		fmt.Println("error parsing html template:", err)
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header["Refferer"])
+		hosts := r.Header["Refferer"]
+		var host string
+		if len(hosts) == 0 {
+			host = "localhost"
+		} else {
+			host = hosts[0]
+		}
+
+		if r.Method == http.MethodPost {
+			if r.FormValue("url") == "" {
+				fmt.Println("error on post: url parameter missing")
+				return
+			}
+			short := shorten(r.FormValue("url"))
+			links[short] = r.FormValue("url")
+
+			fmt.Fprintf(
+				w,
+				"%s/?%s",
+				host,
+				short,
+			)
+			return
+		}
+
+		if r.Method == http.MethodGet && r.URL.RawQuery != "" {
+
+			if err := getTemplate.Execute(w, extend(r.URL.RawQuery)); err != nil {
+				fmt.Println("error rendering get template:", err)
+			}
+			return
+		}
+
+		if err := postTemplate.Execute(w,
+			struct {
+				Links map[string]string
+				Host  string
+			}{
+				Links: links,
+				Host:  host,
+			}); err != nil {
+			fmt.Println("error rendering post template:", err)
+			return
 		}
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
 }
